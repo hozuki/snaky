f12979abc953af1f6c9e51b5d1e2fde3ac7dcac8
George Wu <uiharu@buaa.edu.cn>
2018-04-02 01:30:05 +0200

    Removed build results.

--- out/src/BvspConstants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BvspConstants;
-(function (BvspConstants) {
-    BvspConstants.contentType = "application/jsonrpc-bvsp; charset=utf-8";
-})(BvspConstants = exports.BvspConstants || (exports.BvspConstants = {}));
-//# sourceMappingURL=BvspConstants.js.map
